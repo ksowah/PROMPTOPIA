3e4c28a4750f9909f10ae17e1dc291b96a551b0b
ksowah <sowahkelvin640@gmail.com>
2023-06-30 00:20:51 +0000

    change wording

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,11 @@ const Home = () => {
 
         <br className="max-md:hidden"/>
 
-        <span className="orange_gradient text-center" >AI-Powered Prompts</span>
+        <span className="orange_gradient text-center" >User-Powered Prompts</span>
       </h1>
 
       <p className="desc text-center" >
-        Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts.
+          Promptopia enables users to create and publish tech-related prompts for others to read and engage with. Users can express their creativity by crafting prompts covering various technology topics.
       </p>
 
       {/* FEED */}
